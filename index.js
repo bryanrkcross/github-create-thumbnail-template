3c5598fee25f44a2f9f7faf6cdd928b494bea69b
Bryan Cross <bryancross@github.com>
2017-10-25 19:27:39 -0500

    what

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var gm = require('gm')
 var util = require('util');
 
 // constants
-var MAX_WIDTH  = 500;
-var MAX_HEIGHT = 500;
+var MAX_WIDTH  = 100;
+var MAX_HEIGHT = 100;
 
 // get reference to S3 client
 var s3 = new AWS.S3();
